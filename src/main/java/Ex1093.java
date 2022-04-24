c694a422c837c4ccdc7c1eee3f7549617941bd9d
Joohyegyeong <hyegyeong25@gmail.com>
2022-04-24 23:41:10 +0900

    22.04.24- code up

---
+++ src/main/java/Ex1093.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex1093 {
+    public static void main(String[] args){
+        //첫 번째 줄에 출석 번호를 부른 횟수인 정수 n이 입력된다. (1 ~ 10000)
+        //두 번째 줄에는 무작위로 부른 n개의 번호(1 ~ 23)가 공백을 두고 순서대로 입력된다.
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int[] nArr = new int[n1];
+        int[] nCnt = new int[n1];
+        for(int i = 0; i < nArr.length; i++){
+            nArr[i] = scan.nextInt();
+            nCnt[i] = 0;
+        }
+        for(int i = 0; i < nCnt.length; i++){
+            int cnt = nArr[i];
+            nCnt[cnt-1]++;
+        }
+        System.out.println(Arrays.toString(nCnt));
+    }
+}
