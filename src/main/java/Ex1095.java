6c5a30fb80cee2550c75cf020a3dda760b4fd27e
Joohyegyeong <hyegyeong25@gmail.com>
2022-04-26 17:20:52 +0900

    22.04.26- code up

--- src/main/java/Ex1095.java
+++ src/main/java/Ex1095.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+public class Ex1095 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] k = new int[n];
-
         //min 값
         for(int i = 0; i < n; i++){
             k[i] = scan.nextInt();
         }
-
         int temp = k[0];
         for(int i = 0; i < k.length; i++){
             if(k[i] < temp){
